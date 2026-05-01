ed48ecb0dbad12d2e05069649086cc401d3364dd
Vinicius <viniciusdejjesuspessoal@gmail.com>
2026-05-01 11:13:56 -0300

    entidades

---
+++ src/main/java/vinix/entities/Conta.java
@@ -0,0 +1,69 @@
+package vinix.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_conta")
+public class Conta implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Integer numeroConta;
+	private User pessoa;
+	private Double saldo = 0.0;
+	
+	public Conta() {}
+
+	public Conta(Integer numeroConta, User pessoa, Double saldo) {
+		super();
+		this.numeroConta = numeroConta;
+		this.pessoa = pessoa;
+		this.saldo = saldo;
+	}
+
+	public Integer getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(Integer numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public User getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(User pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(numeroConta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		return Objects.equals(numeroConta, other.numeroConta);
+	}
+}
--- src/main/java/vinix/entities/User.java
+++ src/main/java/vinix/entities/User.java
@@ -1,5 +1,69 @@
 package vinix.entities;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String name;
+	private String cpf;
+	private String email;
+
+	public User() {}
+
+	public User(String name, String cpf, String email) {
+		super();
+		this.cpf = cpf;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
 }
